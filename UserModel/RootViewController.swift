76b58053486449d7a09a2575ec0ecb30f0e88732
Alex <therebeloper@gmail.com>
2019-04-13 09:38:11 +0300

    Initial Commit

---
+++ UserModel/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  UserModel
+//
+//  Created by Alex Nagy on 13/04/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
